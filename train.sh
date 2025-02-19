b90d653aa13042b043f243c29b8685890085bc71
Yue Gao <hologerry@gmail.com>
2025-02-20 02:46:59 +0800

    update

--- train.sh
+++ train.sh
@@ -1,5 +1,8 @@
 {
 python train.py \
+    --s /data/Dynamics/RealCaptureBlackBlueCloudOneExtraData \
+    --config configs/gaussian_pbd_fluid/real_capture_gs_blue_extra.json \
+    --loader real_capture_extra \
     -s /data/Dynamics/RealCaptureBlackCloudOneData \
     -m /data/Dynamics/gaussian_splatting_logs/real_capture_black_cloud_one/frame_0_debug_all_view \
     --eval \
